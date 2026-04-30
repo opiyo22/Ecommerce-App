bcedfe1489d9d6af449dc33d2b94ca54cf3be2f8
opiyo22 <billycopiyo22@gmail.com>
2026-04-30 22:59:08 +0300

    shop page UI completed

--- client/app/product/[id].tsx
+++ client/app/product/[id].tsx
@@ -33,13 +33,15 @@ export default function ProductDetails() {
   const [activeImageIndex, setActiveImageIndex] = useState(0);
 
   const fetchProduct = async () => {
-    setProduct(dummyProducts.find((product) => product._id === id) as any);
+    // setProduct(dummyProducts.find((product) => product._id === id) as any);
+    const found: any = dummyProducts.find((product) => product._id === id);
+    setProduct(found ?? null);
     setLoading(false);
   };
 
   useEffect(() => {
     fetchProduct();
-  }, []);
+  }, [id]);
   if (loading) {
     return (
       <SafeAreaView className="flex-1 justify-center items-center">
